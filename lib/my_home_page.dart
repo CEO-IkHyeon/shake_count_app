42f9f44822d36d88ab70bb480c471d49b43b3d40
Ik Hyeon Cho <andantej99@ajou.ac.kr>
2023-07-29 11:40:55 +0900

    03_01_07~09_appCycle 및 여러 외부 패키지 사용법 1. AppLifeCycleState 설정하는 법 2. velocity_x 패키지 사용법 3. flutter_native_splash 패키지 사용법

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shake/shake.dart';
+import 'package:shake_count_app/red_box.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -20,12 +22,19 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   int _counter = 0;
+  late ShakeDetector detector;
 
   @override
   void initState() {
-    ShakeDetector detector = ShakeDetector.autoStart(
+    //addObserver에 관찰할 대상을 넣어줘야함 -> 해당 class는 State이기에 this를 넣어도 WidgetsBinding으로 인식안함
+    // -> 그래서 'with WidgetsBindingObserver' 를 추가 -> '_MyHomePageState' 자체가 'WidgetsBindingObserver로 인식' (믹싱 형태로 구현)
+    // with 대신 implements 사용 가능하지만 모든 함수를 override해서 구현해야함 -> 불필요
+    WidgetsBinding.instance.addObserver(this);
+
+    // initState에서 생성되어 하단의 lifecycle에서도 사용가능
+    detector = ShakeDetector.autoStart(
       onPhoneShake: () {
         setState(() => _counter++);
       },
@@ -34,56 +43,67 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
+  @override
+  void dispose() {
+    // 위에서 초기화할 때 listener 열어주고
+    // 닫을 때 remove를 해야 리소스가 빠져나갈 가능성을 사전에 방지
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              '흔들어서 카운트를 올려보세요.',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const RedBox(),
+                // Container(
+                //   color: Colors.red,
+                //   width: 20,
+                //   height: 20,
+                // ),
+                Column(
+                  children: [
+                    // redbox 주변으로 30의 여백이 있는 파란색 박스 만들기
+                    const RedBox().box.padding(EdgeInsets.all(30)).color(Colors.blue).make(),
+                    // Text() 위젯을 가독성있게 바꿀 수 있음 -> 마지막에 선언된 색이 적용된다
+                    // 첫번째 .make를 했을 때 Text() 위젯을 반환하는 것이 아닌 'AutoSizeText'를 반환한다
+                    // -> 'Text'위젯을 반환하게 하려면 .make() 이전에 _isIntrinsic()를 호출하면 된다
+                    '흔들어서 카운트를 올려보세요.'
+                        .text
+                        .size(20)
+                        .color(Colors.red)
+                        .bold
+                        .white
+                        .black
+                        .isIntrinsic
+                        .makeCentered() // 텍스트가 중앙에 오게
+                        .box // 여기서부턴 박스 관련 호출들
+                        .withRounded(value: 50) //박스 둥글게 만듦
+                        .color(Colors.green)
+                        .height(150)
+                        .make()
+                        .pSymmetric(h: 20, v: 50),
+                    const RedBox(),
+                  ],
+                ),
+                const RedBox(),
+              ],
             ),
             Text(
               '$_counter',
@@ -98,5 +118,22 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  // 앱 lifecycle이 바뀔 때 어떤 처리를 할 수 있는지
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        detector.startListening();
+        break;
+      case AppLifecycleState.inactive:
+        break;
+      case AppLifecycleState.detached:
+        break;
+      case AppLifecycleState.paused:
+        detector.stopListening();
+        break;
+    }
   }
 }
